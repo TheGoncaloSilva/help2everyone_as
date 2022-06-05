75da5b614dae3eedc2ab7728e167de6ebb0ba925
Gonçalo <goncalo.lslv.silva@gmail.com>
2022-06-05 12:45:16 +0100

    Frontend and Backend demo created using HTML and DJANGO templates,still ongoing the development of the prototype interaction, volunteer subscription to an event

--- help2everyone/apps/main/urls.py
+++ help2everyone/apps/main/urls.py
@@ -5,6 +5,22 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('hello', views.say_hello),
-    path('', views.index),
+    path('hello', views.say_hello, name='hello'),
+    path('test', views.test, name='test'),
+
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+
+    path('about', views.about, name='about'),
+    path('about.html', views.about, name='about'),
+
+    path('blog', views.blog, name='blog'),
+    path('blog.html', views.blog, name='blog'),
+
+    path('events', views.events, name='events'),
+    path('events.html', views.events, name='events'),
+
+    path('contact', views.contact, name='contact'),
+    path('contact.html', views.contact, name='contact'),
 ]
--- help2everyone/apps/main/views.py
+++ help2everyone/apps/main/views.py
@@ -1,10 +1,31 @@
+import os
 from django.shortcuts import render # Render html code
 from django.http import HttpResponse
+from django.contrib.staticfiles import finders
+
+from help2everyone.settings import BASE_DIR, MEDIA_ROOT, PROJECT_ROOT, STATIC_ROOT, STATIC_URL
 
 # Create your views here.
 def say_hello(request):
     # pull data from db, transform, send emil...
     return HttpResponse('Hello World')
 
+def test(request):
+    # pull data from db, transform, send emil...
+    context = {'value' : ['good', 'meh', 'bad']}
+    return render(request, 'test.html', context)
+
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def events(request):
+    return render(request, 'events.html')
+
+def contact(request):
+    return render(request, 'contact.html')
