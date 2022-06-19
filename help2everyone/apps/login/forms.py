b025684d5e17de034298443eb18efbf7a0bb0245
Gonçalo <goncalo.lslv.silva@gmail.com>
2022-06-19 15:38:41 +0100

    Account registry developed for organization and corrected some developments in voluntary account creation

--- help2everyone/apps/login/forms.py
+++ help2everyone/apps/login/forms.py
@@ -14,10 +14,14 @@ class RegisterVol(forms.Form):
     ageOfBirth = forms.DateField(label='Birthdate', initial=datetime.date.today, widget = DateInput)
     image = forms.ImageField(label='Profile Image', required=False)
     password = forms.CharField(label='Password', widget = forms.PasswordInput)
-    confirmPassword = forms.CharField(label='Confirmar Password', widget = forms.PasswordInput)
+    confirmPassword = forms.CharField(label='Confirm Password', widget = forms.PasswordInput)
 
-
-
-    #data = registerVolForm.cleaned_data['ageOfBirth']
-    #        if data > datetime.date.today():
-    #            raise forms.ValidationError("'to' date cannot be later than today.")
+class RegisterOrg(forms.Form):
+    email = forms.EmailField(label='Email')
+    name = forms.CharField(label='Company Name', max_length=100)
+    image = forms.ImageField(label='Company Logo', required=False)
+    contactNumber = forms.IntegerField(label='Contact Number')
+    websiteAddress = forms.URLField(label='Website Address')
+    foundationDate = forms.DateField(label='Foundation Date', initial=datetime.date.today, widget = DateInput)
+    password = forms.CharField(label='Password', widget = forms.PasswordInput)
+    confirmPassword = forms.CharField(label='Confirm Password', widget = forms.PasswordInput)
--- help2everyone/apps/login/views.py
+++ help2everyone/apps/login/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import logout, login as auth_login, authenticate
 from django.contrib import messages
 from django.http import HttpResponse
 from ..main.models import Voluntary, Organization
-from .forms import RegisterVol
+from .forms import RegisterVol, RegisterOrg
 import datetime
 from django import forms
 
@@ -101,13 +101,9 @@ def register_vol(request):
                 vol.save()
                 return redirect('login_vol')
         
-            #registerVolForm.save()
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            #return redirect('index')
         else:
             print(registerVolForm)
+            context.update({'messages': "Ocorreu um erro ao criar a conta, por favor tente novamente"})
     else:
         form = RegisterVol()
         context.update({'form': form})
@@ -116,4 +112,59 @@ def register_vol(request):
 
 
 def register_org(request):
-    return render(request, 'register_org.html')
+    """errors = ["Utilizador já registado!!!",	"Email já registado!!!"
+        "Palavra-Passe fraca!!!", "As Palavras-Passe não coincidem, por favor tente novamente!!!", 
+        "Ocorreu um erro ao criar a conta, por favor tente novamente!!!"]"""
+    context = {}
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        registerOrgForm = RegisterOrg(request.POST, request.FILES)
+        # check whether it's valid:
+        if registerOrgForm.is_valid() or form['image'] is None:
+            form = registerOrgForm.cleaned_data
+            print(form)
+            if User.objects.filter(email=form['email']).exists():
+                context.update({'messages': "Email já registado"})
+                context.update({'form': RegisterOrg(request.POST)})
+            elif form['password'] != form['confirmPassword']:
+                context.update({'messages': "As Palavras-Passe não coincidem, por favor tente novamente"})
+                context.update({'form': RegisterOrg(request.POST)})
+            elif len(form['password']) < 4:
+                context.update({'messages': "Palavra-Passe fraca"})
+                context.update({'form': RegisterOrg(request.POST)})
+            else:
+                user = User.objects.create_user(username = form['email'], 
+                                            email = form['email'],
+                                            first_name = form['name'], 
+                                            password = form['password']
+                                            )
+                user.save()
+                if form['image'] is None:
+                    org = Organization.objects.create(orgName = form['name'],
+                            email = form['email'],
+                            contactNumber = form['contactNumber'],
+                            websiteAddress = form['websiteAddress'],
+                            foundationDate = form['foundationDate'],
+                            registryDate = datetime.date.today(),
+                        )
+                else:
+                    org = Organization.objects.create(orgImg = request.FILES.get('image'),
+                            orgName = form['name'],
+                            email = form['email'],
+                            contactNumber = form['contactNumber'],
+                            websiteAddress = form['websiteAddress'],
+                            foundationDate = form['foundationDate'],
+                            registryDate = datetime.date.today(),
+                        )
+
+                org.save()
+                return redirect('login_org')
+
+        else:
+            print(registerOrgForm)
+            context.update({'messages': "Ocorreu um erro ao criar a conta, por favor tente novamente"})
+    else:
+        form = RegisterOrg()
+        context.update({'form': form})
+
+    return render(request, 'register_org.html', context)
