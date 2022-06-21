2d62358c5896eb527215e62c1ee3fdb783a80e18
Gonçalo <goncalo.lslv.silva@gmail.com>
2022-06-21 20:37:56 +0100

    Small change for heroku for validation

--- help2everyone/settings.py
+++ help2everyone/settings.py
@@ -113,6 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
+CSRF_TRUSTED_ORIGINS = ["https://help2everyone.herokuapp.com/"]
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
